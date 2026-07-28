dd04cc23c00536b326b26a294ba0dc8cd577d77f
venslyy <hackerbro228007@mail.ru>
2026-07-28 19:33:53 +0300

    Add static analysis with Larastan

--- app/Models/LoginSession.php
+++ app/Models/LoginSession.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// <-- вот этой строки, похоже, не хватает
+/**
+ * @property User|null $user
+ */
 
 class LoginSession extends Model
 {
